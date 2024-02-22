48be47f566b313779ccabbffd0fa1e10a8b53a4d
Alvaro-IGJU <127084235+Alvaro-IGJU@users.noreply.github.com>
2024-02-22 17:13:34 +0100

    working on view

---
+++ resources/views/list.blade.php
@@ -0,0 +1,41 @@
+<section class="listHouses">
+    <div class="presentation">
+        <h2>Bienvenido a mi sitio</h2>
+        <p>Esta es una sección de ejemplo. Puedes modificarla según tus necesidades.</p>
+    </div>
+    <div class="list-houses">
+        <div class="card-house">
+            <img src="https://tripkay.com/destination-guides/wp-content/uploads/2017/03/Playa-de-Hospitalet-del-Infant-1620x1080.jpg.webp"
+                alt="">
+                <h2>Hospitalet de l'infant</h2>
+
+            <div class="specs">
+                <div><i class="fa-solid fa-bed"></i>1 habitación</div>
+                <div><i class="fa-solid fa-plate-wheat"></i>Cocina</div>
+                <div><i class="fa-solid fa-bath"></i>1 baño</div>
+                <div><i class="fa-solid fa-ruler-combined"></i>100 m²</div>
+                <div><i class="fa-solid fa-elevator"></i>Ascensor</div>
+                <div><i class="fa-solid fa-person-through-window"></i>Balcón</div>
+                <div><i class="fa-solid fa-fan"></i>Aire acondicionado</div>
+
+            </div>
+        </div>
+        <div class="card-house">
+            <img src="https://tripkay.com/destination-guides/wp-content/uploads/2017/03/Playa-de-Hospitalet-del-Infant-1620x1080.jpg.webp"
+                alt="">
+                <h2>Calafat</h2>
+
+            <div class="specs">
+                <div><i class="fa-solid fa-bed"></i> 1 habitación</div>
+                <div><i class="fa-solid fa-plate-wheat"></i>Cocina</div>
+                <div><i class="fa-solid fa-bath"></i> 1 baño</div>
+                <div><i class="fa-solid fa-ruler-combined"></i>100 m²</div>
+                <div><i class="fa-solid fa-elevator"></i>Ascensor</div>
+                <div><i class="fa-solid fa-person-through-window"></i>Balcón</div>
+                <div><i class="fa-solid fa-fan"></i>Aire acondicionado</div>
+
+            </div>
+        </div>
+    </div>
+
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,30 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenido</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-</head>
-<body class="bg-gray-100">
-    <header class="bg-gray-800 text-white py-4">
-        <div class="container mx-auto flex justify-between items-center px-4">
-            <nav>
-                <ul class="flex space-x-4">
-                    <li><a href="#" class="hover:text-gray-300">Inicio</a></li>
-                    <li><a href="#" class="hover:text-gray-300">Acerca</a></li>
-                    <li><a href="#" class="hover:text-gray-300">Contacto</a></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <title>Hospitalet de l'Infant</title>
+    <!DOCTYPE html>
+    <html lang="es">
 
-    <section class="container mx-auto mt-8">
-        <div class="px-4">
-            <h2 class="text-2xl font-bold mb-4">Bienvenido a mi sitio</h2>
-            <p class="text-gray-800">Esta es una sección de ejemplo. Puedes modificarla según tus necesidades.</p>
-        </div>
-    </section>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bienvenido</title>
+        <link rel="stylesheet" href="{{ url('css/app.css') }}" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-************" crossorigin="anonymous" />
 
-    <script src="{{ asset('js/app.js') }}"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+
+<body>
+    @include('header')
+    <div class="space"></div>
+    
+    @include('list')
+
 </body>
+
 </html>

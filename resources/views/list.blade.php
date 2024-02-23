44820926d314c7b0f7a4f75639883728810542a8
Alvaro-IGJU <127084235+Alvaro-IGJU@users.noreply.github.com>
2024-02-23 16:34:57 +0100

    working on show view

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -7,7 +7,7 @@
         <div class="card-house">
             <img src="https://tripkay.com/destination-guides/wp-content/uploads/2017/03/Playa-de-Hospitalet-del-Infant-1620x1080.jpg.webp"
                 alt="">
-                <h2>Hospitalet de l'Infant</h2>
+            <h2>Hospitalet de l'Infant</h2>
 
             <div class="specs">
                 <div><i class="fa-solid fa-bed"></i>1 habitación</div>
@@ -15,26 +15,35 @@
                 <div><i class="fa-solid fa-bath"></i>1 baño</div>
                 <div><i class="fa-solid fa-ruler-combined"></i>100 m²</div>
                 <div><i class="fa-solid fa-elevator"></i>Ascensor</div>
-                <div><i class="fa-solid fa-person-through-window"></i>Balcón</div>
+                <div> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation"
+                        focusable="false" style="display: block; height: 24px; width: 24px; fill: currentcolor;">
+                        <path
+                            d="M23 1a2 2 0 0 1 2 1.85V19h4v2h-2v8h2v2H3v-2h2v-8H3v-2h4V3a2 2 0 0 1 1.85-2H9zM9 21H7v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm-10-8H9v6h6zm8 0h-6v6h6zM15 3H9v8h6zm8 0h-6v8h6z">
+                        </path>
+                    </svg>
+                    Balcón</div>
                 <div><i class="fa-solid fa-fan"></i>Aire acondicionado</div>
 
             </div>
         </div>
-        <div class="card-house">
-            <img src="https://www.calafat.net/wp-content/uploads/2022/05/llobeta.png"
-                alt="">
-                <h2>Calafat</h2>
+        <a class="card-house" href="{{ url('apartments/1') }}">
+            <img src="{{ asset('uploads/1/Terraza.jpg') }}" alt="">
+            <h2>Calafat</h2>
 
             <div class="specs">
                 <div><i class="fa-solid fa-bed"></i> 1 habitación</div>
                 <div><i class="fa-solid fa-plate-wheat"></i>Cocina</div>
                 <div><i class="fa-solid fa-bath"></i> 1 baño</div>
                 <div><i class="fa-solid fa-ruler-combined"></i>100 m²</div>
-                <div><i class="fa-solid fa-person-through-window"></i>Balcón</div>
+                <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation"
+                        focusable="false" style="display: block; height: 24px; width: 24px; fill: currentcolor;">
+                        <path
+                            d="M23 1a2 2 0 0 1 2 1.85V19h4v2h-2v8h2v2H3v-2h2v-8H3v-2h4V3a2 2 0 0 1 1.85-2H9zM9 21H7v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm-10-8H9v6h6zm8 0h-6v6h6zM15 3H9v8h6zm8 0h-6v8h6z">
+                        </path>
+                    </svg>Balcón</div>
                 <div><i class="fa-solid fa-fan"></i>Aire acondicionado</div>
-
             </div>
-        </div>
+        </a>
     </div>
 
 </section>

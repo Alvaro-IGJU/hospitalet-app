fb029bc839dec4c86a6bd12124d0b13126aee50a
Álvaro Iglesias Jusmet <alvaro.iglesias.dev@gmail.com>
2024-03-17 23:52:02 +0100

    practically finished frontend apartment

--- resources/views/apartments/show.blade.php
+++ resources/views/apartments/show.blade.php
@@ -7,16 +7,20 @@
     <title>Bienvenido</title>
     <link rel="stylesheet" href="{{ url('css/app.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ url('css/header_footer.css') }}" type="text/css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-**" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-**" crossorigin="anonymous" />
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Agrega las bibliotecas de Fancybox -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css" />
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
     <link rel="stylesheet" href="{{ url('css/show.css') }}" type="text/css">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin="" />
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin="" />
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.11/index.global.min.js'></script>
@@ -33,19 +37,21 @@
         <h2 class="title-house">{{ $apartment->name }}</h2>
         <div class="photos">
             @foreach ($photos as $index => $photo)
-            @if ($index == 0)
-            <a href="{{ asset($photo) }}" data-fancybox="gallery" class="first-photo">
-                <img src="{{ asset($photo) }}" alt="Foto principal del apartamento">
-            </a>
-            @elseif ($index > 0 && $index < 5) @if ($index==1) <div class="side-photos">
-                @endif
-                <a href="{{ asset($photo) }}" data-fancybox="gallery" class="side-photo">
-                    <img src="{{ asset($photo) }}" alt="Foto del apartamento">
-                </a>
-                @if ($index == 4)
+                @if ($index == 0)
+                    <a href="{{ asset($photo) }}" data-fancybox="gallery" class="first-photo">
+                        <img src="{{ asset($photo) }}" alt="Foto principal del apartamento">
+                    </a>
+                @elseif ($index > 0 && $index < 5)
+                    @if ($index == 1)
+                        <div class="side-photos">
+                    @endif
+                    <a href="{{ asset($photo) }}" data-fancybox="gallery" class="side-photo">
+                        <img src="{{ asset($photo) }}" alt="Foto del apartamento">
+                    </a>
+                    @if ($index == 4)
         </div>
         @endif
-        @else
+    @else
         <a hidden href="{{ asset($photo) }}" data-fancybox="gallery" class="first-photo">
             <img src="{{ asset($photo) }}" alt="Foto principal del apartamento">
         </a>
@@ -131,8 +137,9 @@
                 </div>
                 <div class="separator-spec"></div>
                 <div class="individual-spec">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation"
-                        focusable="false" style="display: block; height: 24px; width: 24px; fill: currentcolor;">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true"
+                        role="presentation" focusable="false"
+                        style="display: block; height: 24px; width: 24px; fill: currentcolor;">
                         <path
                             d="M11 1v7l1.9 20.82v.17a2.01 2.01 0 0 1-1.81 2 2 2 0 0 1-.18.01H2.92a2.01 2.01 0 0 1-1.82-2v-.09l1.9-21V1zm6 0h.15a2 2 0 0 1 1.84 1.84L19 3v7.12l-2 8V31h-2V17.96l.03-.16.03-.16L16.72 11H13V1zm11 0a2 2 0 0 1 2 1.85V29a2 2 0 0 1-1.85 2H21v-2h7v-2h-7v-2h7v-2h-7v-2h7v-2h-7v-2h7v-2h-7v-2h7v-2h-7V9h7V7h-7V5h7V3h-7V1zM9.09 9H4.9L3.1 29h7.81v-.06zM17 3h-2v6h2zM9 3H5v4h4z">
                         </path>
@@ -454,44 +461,51 @@
         <div id="datePickDiv">
             <div id="fixedCalendar">
 
- 
-            <div id='calendar'></div>
-            <div id="dateSelector">
-                <div id="priceNight">
-                    <b id="weekNightPrice"></b> 
-                </div>
-                <div class="priceGenerator" id="priceGenerator">
-                    <div class="border-price date">
-                        <div class="firstDay">
-                            <div class="selectorTitle"><p>LLEGADA</p></div>
-                            <div id="firstDay"></div>
-                        </div>
-                        <div class="finalDay">
-                            <div class="selectorTitle"><p>SALIDA</p></div>
-                            <div  id="finalDay"></div>
-                        </div>
+
+                <div id='calendar'></div>
+                <div id="dateSelector">
+                    <div id="priceNight">
+                        <b id="weekNightPrice">Seleccionar semana</b>
                     </div>
-                    <div id="datesOptions" class="datesOptions" style="display: none;"></div>
-                  
-                  
+                    <div class="priceGenerator" id="priceGenerator">
+                        <div class="border-price date">
+                            <div class="firstDay">
+                                <div class="selectorTitle">
+                                    <p>LLEGADA</p>
+                                </div>
+                                <div id="firstDay"></div>
+                            </div>
+                            <div class="finalDay">
+                                <div class="selectorTitle">
+                                    <p>SALIDA</p>
+                                </div>
+                                <div id="finalDay"></div>
+                            </div>
+                        </div>
+                        <div id="datesOptions" class="datesOptions" style="display: none;"></div>
+
+
+
+                    </div>
+                    <div class="separator-date">
+
+                    </div>
+                    <div class="totalPrice">
+                        <div>Total</div>
+                        <div id="totalPrice"></div>
+                    </div>
 
                 </div>
-                <div class="separator-date">
-
-                </div>
-                <div class="totalPrice">
-                    <div>Total</div>
-                    <div id="totalPrice"></div>
-                </div>
-
             </div>
         </div>
-        </div>
     </section>
-        <section id="googleMaps">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12057.588654651208!2d0.8369571726501279!3d40.92896200360809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a115c7152cc26f%3A0x7997ac69c89313b3!2sCalafat%2C%20Tarragona!5e0!3m2!1ses!2ses!4v1709579150804!5m2!1ses!2ses" width="1600" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <section id="googleMaps">
+        <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12057.588654651208!2d0.8369571726501279!3d40.92896200360809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12a115c7152cc26f%3A0x7997ac69c89313b3!2sCalafat%2C%20Tarragona!5e0!3m2!1ses!2ses!4v1709579150804!5m2!1ses!2ses"
+            width="1600" height="600" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
 
-        </section>
+    </section>
     <div class="separator"></div>
     <section class="rules">
         <h4>Qué debes saber</h4>
@@ -538,6 +552,7 @@
             </div>
         </div>
     </section>
+    @include('footer')
 
 
     <script src="{{ asset('js/calendar.js') }}"></script>

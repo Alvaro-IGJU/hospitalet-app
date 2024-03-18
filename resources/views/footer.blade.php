d732c8dbdd45ef86a6d7cb923e82f468bf5ca9ee
Álvaro Iglesias Jusmet <alvaro.iglesias.dev@gmail.com>
2024-03-18 12:36:27 +0100

    better footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,9 +2,9 @@
     <a class="icon" href="">CALAFAT</a>
     <nav>
         <ul class="">
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Acerca</a></li>
-            <li><a href="#">Contacto</a></li>
+            {{-- <li><a href="#">Inicio</a></li> --}}
+            {{-- <li><a href="#">Acerca</a></li> --}}
+            <li><p>Desarrolladores WEB</p><a href="https://www.linkedin.com/in/%C3%A1lvaro-iglesias-jusmet/">Álvaro Iglesias</a><a href="https://www.linkedin.com/in/salim-tamazirt-653421256/">Salim Tamazirt</a></li>
         </ul>
     </nav>
 

8ca4ebce9215b2d4fe783c575f004058e380a820
Alvaro-IGJU <127084235+Alvaro-IGJU@users.noreply.github.com>
2024-03-20 18:12:48 +0100

    login fixed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,10 +8,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    public function __construct()
-    {
-        // $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     // $this->middleware('auth');
+    // }
 
     public function read($id = null)
     {
@@ -36,20 +36,28 @@ class AdminController extends Controller
     }
 
     public function loginView(){
+        Auth::logout();
+
         return view('admin.login');
     }
 
     public function login(Request $request){
         // Validar los datos del formulario
-        // dd(password_hash("alvaro", PASSWORD_DEFAULT));
+        $request->validate([
+            'name' => 'required', // Añade las reglas de validación que necesites
+            'password' => 'required' // Por ejemplo, 'required|min:6' para requerir al menos 6 caracteres
+        ]);
+    
         $credentials = $request->only('name', 'password');
+    
         // Intentar autenticar al usuario
         if (Auth::attempt($credentials)) {
             // El usuario está autenticado, redireccionarlo a donde desees
-            return redirect()->intended('admin'); // Cambia 'dashboard' por la URL a la que quieras redirigir al usuario después del inicio de sesión
+            return redirect()->intended('admin'); // Asegúrate de cambiar 'dashboard' por la URL a la que quieras redirigir después del inicio de sesión
         } else {
             // El usuario no está autenticado, mostrar un mensaje de error o redirigirlo de vuelta al formulario de inicio de sesión con un mensaje de error
             return redirect()->route('admin.login')->with('error', 'Credenciales inválidas. Por favor, inténtalo de nuevo.');
         }
     }
+    
 }

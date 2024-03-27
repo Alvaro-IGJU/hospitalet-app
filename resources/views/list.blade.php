e953506f2e09b9ca614f740ededfed61f599530c
Álvaro Iglesias Jusmet <alvaro.iglesias.dev@gmail.com>
2024-03-27 15:24:42 +0100

    better icons

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -9,20 +9,46 @@
             <h2>Loft en Calafat</h2>
 
             <div class="specs">
-                <div><i class="fa-solid fa-bed"></i>1 habitación</div>
-                <div><i class="fa-solid fa-plate-wheat"></i>Cocina</div>
-                <div><i class="fa-solid fa-bath"></i>1 baño</div>
-                <div><i class="fa-solid fa-ruler-combined"></i>100 m²</div>
-                <div><i class="fa-solid fa-elevator"></i>Ascensor</div>
+                <div><i class="fa-solid fa-user-group"></i><p>2 personas</p></div>
                 <div> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation"
-                        focusable="false" style="display: block; height: 24px; width: 24px; fill: currentcolor;">
+                    focusable="false" style="display: block; height: 24px; width: 24px; fill: currentcolor;">
+                    <path
+                        d="M28 2a2 2 0 0 1 2 2v24a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2zm0 2H4v15.5h.19c.37-.04.72-.17 1-.38l.14-.11A3.98 3.98 0 0 1 8 18c.99 0 1.95.35 2.67 1 .35.33.83.5 1.33.5.5 0 .98-.17 1.33-.5A3.97 3.97 0 0 1 16 18c.99 0 1.95.35 2.67 1 .35.33.83.5 1.33.5.5 0 .98-.17 1.33-.5A3.98 3.98 0 0 1 24 18c.99 0 1.94.35 2.67 1 .35.33.83.5 1.33.5v2h-.23a3.96 3.96 0 0 1-2.44-1A1.98 1.98 0 0 0 24 20c-.5 0-.98.17-1.33.5a3.98 3.98 0 0 1-2.67 1 3.98 3.98 0 0 1-2.67-1A1.98 1.98 0 0 0 16 20c-.5 0-.98.17-1.33.5a3.98 3.98 0 0 1-2.67 1 3.98 3.98 0 0 1-2.67-1A1.98 1.98 0 0 0 8 20c-.5 0-.98.17-1.33.5a3.98 3.98 0 0 1-2.67 1v3h.19c.37-.04.72-.17 1-.38l.14-.11A3.98 3.98 0 0 1 8 23c.99 0 1.95.35 2.67 1 .35.33.83.5 1.33.5.5 0 .98-.17 1.33-.5A3.97 3.97 0 0 1 16 23c.99 0 1.95.35 2.67 1 .35.33.83.5 1.33.5.5 0 .98-.17 1.33-.5A3.98 3.98 0 0 1 24 23c.99 0 1.94.35 2.67 1 .35.33.83.5 1.33.5v2h-.23a3.96 3.96 0 0 1-2.44-1A1.98 1.98 0 0 0 24 25c-.5 0-.98.17-1.33.5a3.98 3.98 0 0 1-2.67 1 3.98 3.98 0 0 1-2.67-1A1.98 1.98 0 0 0 16 25c-.5 0-.98.17-1.33.5a3.98 3.98 0 0 1-2.67 1 3.98 3.98 0 0 1-2.67-1A1.98 1.98 0 0 0 8 25c-.5 0-.98.17-1.33.5a3.98 3.98 0 0 1-2.67 1V28h24zm-6 3a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm0 2a1 1 0 1 0 0 2 1 1 0 0 0 0-2z">
+                    </path>
+                </svg>
+                <p>Vistas al mar</p></div>
+                <div><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true"
+                    role="presentation" focusable="false"
+                    style="display: block; height: 24px; width: 24px; fill: currentcolor;">
+                    <path
+                        d="M26 1a5 5 0 0 1 5 5c0 6.39-1.6 13.19-4 14.7V31h-2V20.7c-2.36-1.48-3.94-8.07-4-14.36v-.56A5 5 0 0 1 26 1zm-9 0v18.12c2.32.55 4 3 4 5.88 0 3.27-2.18 6-5 6s-5-2.73-5-6c0-2.87 1.68-5.33 4-5.88V1zM2 1h1c4.47 0 6.93 6.37 7 18.5V21H4v10H2zm14 20c-1.6 0-3 1.75-3 4s1.4 4 3 4 3-1.75 3-4-1.4-4-3-4zM4 3.24V19h4l-.02-.96-.03-.95C7.67 9.16 6.24 4.62 4.22 3.36L4.1 3.3zm19 2.58v.49c.05 4.32 1.03 9.13 2 11.39V3.17a3 3 0 0 0-2 2.65zm4-2.65V17.7c.99-2.31 2-7.3 2-11.7a3 3 0 0 0-2-2.83z">
+                    </path>
+                </svg>
+                <p>Mini-Kitchen</p></div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true"
+                        role="presentation" focusable="false"
+                        style="display: block; height: 24px; width: 24px; fill: currentcolor;">
                         <path
                             d="M23 1a2 2 0 0 1 2 1.85V19h4v2h-2v8h2v2H3v-2h2v-8H3v-2h4V3a2 2 0 0 1 1.85-2H9zM9 21H7v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm4 0h-2v8h2zm-10-8H9v6h6zm8 0h-6v6h6zM15 3H9v8h6zm8 0h-6v8h6z">
                         </path>
                     </svg>
-                    Balcón</div>
-                <div><i class="fa-solid fa-fan"></i>Aire acondicionado</div>
-
+                    <p>Patio</p>
+                </div>
+                <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true"
+                        role="presentation" focusable="false"
+                        style="display: block; height: 24px; width: 24px; fill: currentcolor;">
+                        <path
+                            d="M9 29v-2h2v-2H6a5 5 0 0 1-5-4.78V8a5 5 0 0 1 4.78-5H26a5 5 0 0 1 5 4.78V20a5 5 0 0 1-4.78 5H21v2h2v2zm10-4h-6v2h6zm7-20H6a3 3 0 0 0-3 2.82V20a3 3 0 0 0 2.82 3H26a3 3 0 0 0 3-2.82V8a3 3 0 0 0-2.82-3z">
+                        </path>
+                    </svg>
+                    <p>TV</p>
+                </div>
+                <div>
+                    <svg  enable-background="new 0 0 510 510" viewBox="0 0 510 510" xmlns="http://www.w3.org/2000/svg" style="display: block; height: 30px; width: 30px; fill: currentcolor;">><g><path d="m465 97.5c-41.355 0-75 33.645-75 75v15h-75.25c-24.675 0-44.75 20.075-44.75 44.75v15.25h-225c-24.814 0-45 20.186-45 45v120h30v-75h240v75h240v-315zm-45 75c0-24.813 20.186-45 45-45h15v120h-60v-60zm-120 59.75c0-8.133 6.617-14.75 14.75-14.75h75.25v30h-90zm-270 75.25v-15c0-8.272 6.728-15 15-15h225v30zm270 75v-105h180v105z"/></g></svg>
+                    <p>Sofá cama</p>
+                </div>
             </div>
         </a>
         <a class="card-house" href="{{ url('apartments/1') }}">
